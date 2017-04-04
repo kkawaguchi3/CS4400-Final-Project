12230155e48b11549f35a59f5e94face7088cbd0
kkawaguchi3 <kkawaguchi3@gatech.edu>
2017-04-04 15:26:18 -0400

    added main app class

---
+++ src/main.java/application/mainApplication.java
@@ -0,0 +1,17 @@
+package main.java.Application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class mainApplication extends Application {
+
+
+    public static void main(String[] args) { launch(args); }
+    
+}
+
