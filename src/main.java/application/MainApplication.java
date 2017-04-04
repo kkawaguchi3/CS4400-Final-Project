04f141b34c5dc4e6372df07a29bdaa469388eb7d
kkawaguchi3 <kkawaguchi3@gatech.edu>
2017-04-04 15:33:04 -0400

    fixed naming convention

--- src/main.java/application/MainApplication.java
+++ src/main.java/application/MainApplication.java
@@ -8,10 +8,16 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
+/**
+ * The main application, which is responsible for handling the JavaFX stage. The application
+ * will add and remove screens, load the FXML files, and display controllers depending on the
+ * state of the application.
+ * @author Kenta Kawaguchi
+ * @version 1.0
+ */
 public class mainApplication extends Application {
 
 
     public static void main(String[] args) { launch(args); }
-    
-}
 
+}
